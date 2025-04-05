83c1f7c6c6d9f0231ad046c0905c26bbdd089d0f
Kodai.T <fvbcj53523@yahoo.co.jp>
2025-04-06 00:35:26 +0900

    commit initialize

---
+++ main.py
@@ -0,0 +1,187 @@
+import tkinter as tk
+from tkinter import ttk
+
+# 商品リスト（商品名と価格）
+products = [
+    {"name": "りんご", "price": 100, "category":0},
+    {"name": "コーヒー", "price": 50, "category":1},
+    {"name": "Orange", "price": 80, "category":0},
+    {"name": "ジュース", "price": 150, "category":1},
+]
+categories = [
+    "食品",
+    "ドリンク",
+]
+
+class Register:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("レジ")
+
+
+        self.tab_init()
+        self.product_buttons_set()
+
+
+    def tab_init(self):
+        self.tab_control = ttk.Notebook(root)
+        self.tabs = [
+            {
+                "frame":tk.Frame(self.tab_control),
+                "name":"テーブル1",
+            },
+            {
+                "frame":tk.Frame(self.tab_control),
+                "name":"テーブル2"
+            },
+        ]
+        self.productsOBJs_in_tab = [[], []]
+        self.tabsOBJs = []
+        for i, tab in enumerate(self.tabs):
+            self.tab_control.add(tab["frame"], text = tab["name"])
+            self.tabsOBJs.append(
+                {
+                    "title":tk.Label(tab["frame"], text = str(tab["name"])),
+                    "sum":tk.Label(tab["frame"], text = "SUM",
+                    width=10, anchor="e", font=("Courier", 12))
+                }
+            )
+            self.tabsOBJs[-1]["title"].pack()
+            self.tabsOBJs[-1]["sum"].pack(side=tk.RIGHT)
+
+        self.tab_control.pack(expand=True, fill="both")
+        self.add_product_to_tab(0)
+        self.add_product_to_tab(1)
+        self.add_product_to_tab(2)
+        self.add_product_to_tab(3)
+
+    
+    def calculate_and_print_sum(self, tab_number):
+        sum = 0
+        for productOBJ in self.productsOBJs_in_tab[tab_number]:
+            sum += int(productOBJ["quantity"]["text"]) * int(productOBJ["unit_price"]["text"])
+        print(self.tabsOBJs)
+        self.tabsOBJs[tab_number]["sum"]["text"] = str(sum)     
+
+    def add_product_to_tab(self, prodcut_number):
+        index = self.tab_control.index("current")
+        id = len(self.productsOBJs_in_tab[index])
+        print("index", index)
+        self.productsOBJs_in_tab[index].append(
+            {
+                "frame":tk.Frame(self.tabs[index]["frame"]),
+            }
+        )
+        self.productsOBJs_in_tab[index][-1]["label"] = tk.Label(
+            self.productsOBJs_in_tab[index][-1]["frame"], 
+            text = products[prodcut_number]["name"],
+            width=10,  # 固定幅を設定
+            anchor="e",  # 右寄せ
+            font=("Courier", 12)  # 等幅フォント
+        )
+        self.productsOBJs_in_tab[index][-1]["unit_price"] = tk.Label(
+            self.productsOBJs_in_tab[index][-1]["frame"], 
+            text = str(products[prodcut_number]["price"]),
+            width=10,  # 固定幅を設定
+            anchor="e",  # 右寄せ
+            font=("Courier", 12)  # 等幅フォント
+        )
+        self.productsOBJs_in_tab[index][-1]["minus_button"] = tk.Button(
+            self.productsOBJs_in_tab[index][-1]["frame"], 
+            text = "-",
+            command = lambda:self.minus_product_in_tab(id)
+        )
+        self.productsOBJs_in_tab[index][-1]["quantity"] = tk.Label(
+            self.productsOBJs_in_tab[index][-1]["frame"], 
+            text = 1,
+            width=1,  # 固定幅を設定
+            anchor="e",  # 右寄せ
+            font=("Courier", 12)  # 等幅フォント
+        )
+        self.productsOBJs_in_tab[index][-1]["plus_button"] = tk.Button(
+            self.productsOBJs_in_tab[index][-1]["frame"], 
+            text = "+",
+            command = lambda:self.plus_product_in_tab(id)
+        )
+        self.productsOBJs_in_tab[index][-1]["sum"] = tk.Label(
+            self.productsOBJs_in_tab[index][-1]["frame"],
+            width=10,  # 固定幅を設定
+            anchor="e",  # 右寄せ
+            font=("Courier", 12),  # 等幅フォント
+            text = str(products[prodcut_number]["price"])
+        )
+
+        self.productsOBJs_in_tab[index][-1]["frame"].pack()
+        self.productsOBJs_in_tab[index][-1]["label"].pack(side=tk.LEFT)
+        self.productsOBJs_in_tab[index][-1]["unit_price"].pack(side=tk.LEFT)
+        self.productsOBJs_in_tab[index][-1]["minus_button"].pack(side=tk.LEFT)
+        self.productsOBJs_in_tab[index][-1]["quantity"].pack(side=tk.LEFT)
+        self.productsOBJs_in_tab[index][-1]["plus_button"].pack(side=tk.LEFT)
+        self.productsOBJs_in_tab[index][-1]["sum"].pack(side=tk.LEFT)
+        
+        self.calculate_and_print_sum(self.tab_control.index("current"))
+
+    def plus_product_in_tab(self,i):
+        print("i", i-1)
+        index = self.tab_control.index("current")
+        self.productsOBJs_in_tab[index][i]["quantity"]["text"] = str( int(self.productsOBJs_in_tab[index][i]["quantity"]["text"]) + 1 )
+        self.productsOBJs_in_tab[index][i]["sum"]["text"] = str( int(self.productsOBJs_in_tab[index][i]["unit_price"]["text"]) * int(self.productsOBJs_in_tab[index][i]["quantity"]["text"]))
+        self.calculate_and_print_sum(index)
+
+    def minus_product_in_tab(self,i):
+        index = self.tab_control.index("current")
+        self.productsOBJs_in_tab[index][i]["quantity"]["text"] = str( int(self.productsOBJs_in_tab[index][i]["quantity"]["text"]) - 1 )
+        self.productsOBJs_in_tab[index][i]["sum"]["text"] = str( int(self.productsOBJs_in_tab[index][i]["unit_price"]["text"]) * int(self.productsOBJs_in_tab[index][i]["quantity"]["text"]))
+
+        if int(self.productsOBJs_in_tab[index][i]["quantity"]["text"])<=0:
+            self.erase_product_to_tab(i)
+
+        self.calculate_and_print_sum(index)
+
+    def erase_product_to_tab(self, buyed_product_number):
+        index = self.tab_control.index("current")
+        self.productsOBJs_in_tab[index][buyed_product_number]["label"].destroy()
+        self.productsOBJs_in_tab[index][buyed_product_number]["frame"].destroy()
+        del self.productsOBJs_in_tab[index][buyed_product_number]
+        # ID を更新
+        for new_id, productOBJ in enumerate(self.productsOBJs_in_tab[index]):
+            productOBJ["minus_button"]["command"] = lambda i=new_id: self.minus_product_in_tab(i)
+            productOBJ["plus_button"]["command"] = lambda i=new_id: self.plus_product_in_tab(i)
+
+        self.calculate_and_print_sum(index)
+        
+    def product_buttons_set(self):
+        self.tabs_control_products = ttk.Notebook(root)
+        self.tabs_products = []
+        for category in categories:
+            self.tabs_products.append(
+                {
+                    "frame":tk.Frame(self.tabs_control_products),
+                    "name":category
+                }
+            )
+            self.tabs_control_products.add(self.tabs_products[-1]["frame"], text=str(self.tabs_products[-1]["name"]))
+
+        #self.product_frame = tk.Frame(root)
+        #self.product_frame.pack(side=tk.LEFT, fill=tk.Y)
+        self.products_buttons = []
+        #for products_button in self.products_buttons:
+        #    products_button.destroy()
+        #self.products_buttons = []
+        for i, product in enumerate(products):
+            print(i)
+            self.products_buttons.append(
+                tk.Button(
+                    self.tabs_products[product["category"]]["frame"],
+                    text=f"{product['name']}\n{product['price']}円",
+                    command=lambda i=i:self.add_product_to_tab(i)
+                )
+            )
+            self.products_buttons[-1].pack()
+        self.tabs_control_products.pack(expand=True, fill="both")
+    
+root = tk.Tk()
+Register = Register(root)
+button = tk.Button(root, text="destroy", command = lambda:Register.erase_product_to_tab(1))
+button.pack()
+root.mainloop()
